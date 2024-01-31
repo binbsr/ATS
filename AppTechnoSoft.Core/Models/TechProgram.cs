8f4ae8c1ca3f7c4b57ccf5c5569581d4dd70eba5
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-01-31 21:29:52 +0545

    Delete unnecessary files

--- AppTechnoSoft.Core/Models/TechProgram.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AppTechnoSoft.Interns.Data.Models;
-public class TechProgram : BaseEntity
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Affliation { get; set; } = string.Empty;
-}

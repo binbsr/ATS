8e09a985ac77ea8c74ea9d217826211481f77007
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-01-31 08:55:09 +0545

    Implement team project CRUD

--- AppTechnoSoft.Interns/Data/Models/Project.cs
+++ AppTechnoSoft.Interns/Data/Models/Project.cs
@@ -4,6 +4,6 @@ public class Project
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Requirements { get; set; } = string.Empty;
-    public string RepoUrl { get; set; } = string.Empty;
-    public string BoardUrl { get; set; } = string.Empty;
+    public string? RepoUrl { get; set; }
+    public string? BoardUrl { get; set; }
 }

cd68cf35a8ef9e6b5d10f7fbaa0bdd7b330f04f7
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-02-01 12:07:59 +0545

    Implement instrcutor CRUD

--- AppTechnoSoft.Interns/Data/Models/Instructor.cs
+++ AppTechnoSoft.Interns/Data/Models/Instructor.cs
@@ -1,10 +1,16 @@
-﻿namespace AppTechnoSoft.Interns.Data.Models;
-public class Instrcutor
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppTechnoSoft.Interns.Data.Models;
+public class Instructor
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Resume { get; set; } = string.Empty;
     public string? ProfilePath { get; set; }
+   
+    [NotMapped]
+    public IBrowserFile? Profile { get; set; }
 
     public List<Tag>? Tags { get; set; } = [];
 }

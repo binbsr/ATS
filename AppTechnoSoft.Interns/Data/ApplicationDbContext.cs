9dfb14b551dc43b193d2b8eb39bde360f3bd35ec
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-02-01 10:15:54 +0545

    Add instructor entity

--- AppTechnoSoft.Interns/Data/ApplicationDbContext.cs
+++ AppTechnoSoft.Interns/Data/ApplicationDbContext.cs
@@ -1,4 +1,3 @@
-using AppTechnoSoft.Interns.Components.Account.Pages.Manage;
 using AppTechnoSoft.Interns.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -30,164 +29,168 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
         builder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
 
-        builder.Entity<College>()
-            .HasData([
-                new College { Id = 1, Name = "Mechi Multiple Campus", Location = "Bhadrapur, Jhapa" },
-                new College { Id = 2, Name = "Dhankuta Multiple Campus", Location = "Dhankuta" },
-                new College { Id = 3, Name = "Central Campus of Technology", Location = "Dharan" },
-                new College { Id = 4, Name = "Mahendra Morang Adarsha Multiple Campus", Location = "Biratnagar" },
-                new College { Id = 5, Name = "Mahendra Bindeshwori Multiple Campus", Location = "Rajbiraj" },
-                new College { Id = 6, Name = "Surya Narayan Satya Na. Mo. Yadav Campus", Location = "Siraha" },
-                new College { Id = 7, Name = "Ramsorup Ramsagar Multiple Campus", Location = "Janakpur" },
-                new College { Id = 8, Name = "Thakur Ram Multiple Campus", Location = "Birgunj" },
-                new College { Id = 9, Name = "Birendra Multiple Campus", Location = "Bharatpur" },
-                new College { Id = 10, Name = "Prithivi Narayan Multiple Campus", Location = "Pokhara" },
-                new College { Id = 11, Name = "SidhaNath Science Campus", Location = "Mahendranagar" },
-                new College { Id = 12, Name = "Mahendra Multiple Campus", Location = "Nepalgunj" },
-                new College { Id = 13, Name = "Butwal Multiple Campus", Location = "Butwal" },
-                new College { Id = 14, Name = "Tribhuvan Multiple Campus", Location = "Palpa" },
-                new College { Id = 15, Name = "Tri-Chandra Multiple Campus", Location = "Ghantaghar" },
-                new College { Id = 16, Name = "Amrit Science Campus", Location = "Lainchour" },
-                new College { Id = 17, Name = "Patan Multiple Campus", Location = "Patan" },
-                new College { Id = 18, Name = "Bhaktapur Multiple Campus", Location = "Bhaktapur" },
-                new College { Id = 19, Name = "Padma Kanya Multiple Campus", Location = "Bagbazar" },
-                new College { Id = 20, Name = "Mahendra Multiple Campus", Location = "Ghorahi, Dang" },
-                new College { Id = 21, Name = "Dhaulagiri Campus", Location = "Baglung" },
-                new College { Id = 22, Name = "Gorkha Campus", Location = "Gorkha" },
-                new College { Id = 23, Name = "Bhairahawa Multiple Campus", Location = "Bhairahawa" },
-                new College { Id = 24, Name = "Degree Campus", Location = "Biratnagar" }
-        ]);
+        builder.Entity<Instrcutor>()
+            .HasMany(e => e.Tags)
+            .WithMany();
 
-        builder.Entity<TechProgram>()
-            .HasData([
-                    new TechProgram { Id = 1, Name = "BCA", Affliation = "TU" },
-                new TechProgram { Id = 2, Name = "BCA", Affliation = "PU" },
-                new TechProgram { Id = 3, Name = "BIT", Affliation = "TU" },
-                new TechProgram { Id = 4, Name = "BIT", Affliation = "PU" },
-                new TechProgram { Id = 5, Name = "BIM", Affliation = "TU" },
-                new TechProgram { Id = 6, Name = "BScIT", Affliation = "TU" },
-                new TechProgram { Id = 7, Name = "BScIT", Affliation = "PU" },
-            ]);
-              
+        //builder.Entity<College>()
+        //    .HasData([
+        //        new College { Id = 1, Name = "Mechi Multiple Campus", Location = "Bhadrapur, Jhapa" },
+        //        new College { Id = 2, Name = "Dhankuta Multiple Campus", Location = "Dhankuta" },
+        //        new College { Id = 3, Name = "Central Campus of Technology", Location = "Dharan" },
+        //        new College { Id = 4, Name = "Mahendra Morang Adarsha Multiple Campus", Location = "Biratnagar" },
+        //        new College { Id = 5, Name = "Mahendra Bindeshwori Multiple Campus", Location = "Rajbiraj" },
+        //        new College { Id = 6, Name = "Surya Narayan Satya Na. Mo. Yadav Campus", Location = "Siraha" },
+        //        new College { Id = 7, Name = "Ramsorup Ramsagar Multiple Campus", Location = "Janakpur" },
+        //        new College { Id = 8, Name = "Thakur Ram Multiple Campus", Location = "Birgunj" },
+        //        new College { Id = 9, Name = "Birendra Multiple Campus", Location = "Bharatpur" },
+        //        new College { Id = 10, Name = "Prithivi Narayan Multiple Campus", Location = "Pokhara" },
+        //        new College { Id = 11, Name = "SidhaNath Science Campus", Location = "Mahendranagar" },
+        //        new College { Id = 12, Name = "Mahendra Multiple Campus", Location = "Nepalgunj" },
+        //        new College { Id = 13, Name = "Butwal Multiple Campus", Location = "Butwal" },
+        //        new College { Id = 14, Name = "Tribhuvan Multiple Campus", Location = "Palpa" },
+        //        new College { Id = 15, Name = "Tri-Chandra Multiple Campus", Location = "Ghantaghar" },
+        //        new College { Id = 16, Name = "Amrit Science Campus", Location = "Lainchour" },
+        //        new College { Id = 17, Name = "Patan Multiple Campus", Location = "Patan" },
+        //        new College { Id = 18, Name = "Bhaktapur Multiple Campus", Location = "Bhaktapur" },
+        //        new College { Id = 19, Name = "Padma Kanya Multiple Campus", Location = "Bagbazar" },
+        //        new College { Id = 20, Name = "Mahendra Multiple Campus", Location = "Ghorahi, Dang" },
+        //        new College { Id = 21, Name = "Dhaulagiri Campus", Location = "Baglung" },
+        //        new College { Id = 22, Name = "Gorkha Campus", Location = "Gorkha" },
+        //        new College { Id = 23, Name = "Bhairahawa Multiple Campus", Location = "Bhairahawa" },
+        //        new College { Id = 24, Name = "Degree Campus", Location = "Biratnagar" }
+        //]);
 
-        builder.Entity<Widget>()
-            .HasData([
-                new Widget
-                {
-                    Id = 1,
-                    Title = "CarouselItem",
-                    Description = "An display item in home page",
-                    HtmlContent = "Welcome to AppTechnoSoft! An initiative for students and freshers.",
-                    Created = DateTime.Now,
-                    CreatedBy = "Seed"
-                },
-                new Widget
-                {
-                    Id = 2,
-                    Title = "CarouselItem",
-                    Description = "An display item in home page",
-                    HtmlContent = "Bridging the gap between academia and industry!",
-                    Created = DateTime.Now,
-                    CreatedBy = "Seed"
-                },
-                new Widget
-                {
-                    Id = 3,
-                    Title = "CarouselItem",
-                    Description = "An display item in home page",
-                    HtmlContent = "We guide you on software R&D and SDLC to kickstart your career!",
-                    Created = DateTime.Now,
-                    CreatedBy = "Seed"
-                },
-                new Widget
-                {
-                    Id = 4,
-                    Title = "CarouselItem",
-                    Description = "An display item in home page",
-                    HtmlContent = "Replacement Opportunities!",
-                    Created = DateTime.Now,
-                    CreatedBy = "Seed"
-                },
-                new Widget
-                {
-                    Id = 5,
-                    Title = "CarouselItem",
-                    Description = "An display item in home page",
-                    HtmlContent = "From basics to professional touches!",
-                    Created = DateTime.Now,
-                    CreatedBy = "Seed"
-                },
-                new Widget
-                {
-                    Id = 6,
-                    Title = "Module",
-                    Description = "GIT and GitHub Essentials",
-                    HtmlContent = "todo",
-                    Created = DateTime.Now,
-                    CreatedBy = "Seed"
-                },
-                new Widget
-                {
-                    Id = 7,
-                    Title = "HomeIntro",
-                    Description = "Training + Internship Combo",
-                    HtmlContent = "todo",
-                    Created = DateTime.Now,
-                    CreatedBy = "Seed"
-                }
-            ]);
+        //builder.Entity<TechProgram>()
+        //    .HasData([
+        //            new TechProgram { Id = 1, Name = "BCA", Affliation = "TU" },
+        //        new TechProgram { Id = 2, Name = "BCA", Affliation = "PU" },
+        //        new TechProgram { Id = 3, Name = "BIT", Affliation = "TU" },
+        //        new TechProgram { Id = 4, Name = "BIT", Affliation = "PU" },
+        //        new TechProgram { Id = 5, Name = "BIM", Affliation = "TU" },
+        //        new TechProgram { Id = 6, Name = "BScIT", Affliation = "TU" },
+        //        new TechProgram { Id = 7, Name = "BScIT", Affliation = "PU" },
+        //    ]);
 
-        // Seed roles and admin user
-        string superAdminId = Guid.NewGuid().ToString();
-        string superAdminRoleId = Guid.NewGuid().ToString();
-        string adminRoleId = Guid.NewGuid().ToString();
-        string traineeRoleId = Guid.NewGuid().ToString();
 
-        builder.Entity<IdentityRole>()
-            .HasData([
-                new IdentityRole
-                {
-                    Name = "SuperAdmin",
-                    NormalizedName = "SUPERADMIN",
-                    Id = superAdminRoleId,
-                    ConcurrencyStamp = superAdminRoleId
-                },
-                new IdentityRole
-                {
-                    Name = "Admin",
-                    NormalizedName = "ADMIN",
-                    Id = adminRoleId,
-                    ConcurrencyStamp = adminRoleId
-                },
-                new IdentityRole
-                {
-                    Name = "Trainee",
-                    NormalizedName = "TRAINEE",
-                    Id = traineeRoleId,
-                    ConcurrencyStamp = traineeRoleId
-                }
-            ]);
+        //builder.Entity<Widget>()
+        //    .HasData([
+        //        new Widget
+        //        {
+        //            Id = 1,
+        //            Title = "CarouselItem",
+        //            Description = "An display item in home page",
+        //            HtmlContent = "Welcome to AppTechnoSoft! An initiative for students and freshers.",
+        //            Created = DateTime.Now,
+        //            CreatedBy = "Seed"
+        //        },
+        //        new Widget
+        //        {
+        //            Id = 2,
+        //            Title = "CarouselItem",
+        //            Description = "An display item in home page",
+        //            HtmlContent = "Bridging the gap between academia and industry!",
+        //            Created = DateTime.Now,
+        //            CreatedBy = "Seed"
+        //        },
+        //        new Widget
+        //        {
+        //            Id = 3,
+        //            Title = "CarouselItem",
+        //            Description = "An display item in home page",
+        //            HtmlContent = "We guide you on software R&D and SDLC to kickstart your career!",
+        //            Created = DateTime.Now,
+        //            CreatedBy = "Seed"
+        //        },
+        //        new Widget
+        //        {
+        //            Id = 4,
+        //            Title = "CarouselItem",
+        //            Description = "An display item in home page",
+        //            HtmlContent = "Replacement Opportunities!",
+        //            Created = DateTime.Now,
+        //            CreatedBy = "Seed"
+        //        },
+        //        new Widget
+        //        {
+        //            Id = 5,
+        //            Title = "CarouselItem",
+        //            Description = "An display item in home page",
+        //            HtmlContent = "From basics to professional touches!",
+        //            Created = DateTime.Now,
+        //            CreatedBy = "Seed"
+        //        },
+        //        new Widget
+        //        {
+        //            Id = 6,
+        //            Title = "Module",
+        //            Description = "GIT and GitHub Essentials",
+        //            HtmlContent = "todo",
+        //            Created = DateTime.Now,
+        //            CreatedBy = "Seed"
+        //        },
+        //        new Widget
+        //        {
+        //            Id = 7,
+        //            Title = "HomeIntro",
+        //            Description = "Training + Internship Combo",
+        //            HtmlContent = "todo",
+        //            Created = DateTime.Now,
+        //            CreatedBy = "Seed"
+        //        }
+        //    ]);
 
-        var appUser = new ApplicationUser
-        {
-            Id = superAdminId,
-            Email = "rawal.bishnu@live.com",
-            EmailConfirmed = true,
-            UserName = "rawal.bishnu@live.com",
-            NormalizedUserName = "RAWAL.BISHNU@LIVE.COM"
-        };
+        //// Seed roles and admin user
+        //string superAdminId = Guid.NewGuid().ToString();
+        //string superAdminRoleId = Guid.NewGuid().ToString();
+        //string adminRoleId = Guid.NewGuid().ToString();
+        //string traineeRoleId = Guid.NewGuid().ToString();
 
-        //set user password
-        PasswordHasher<ApplicationUser> ph = new();
-        appUser.PasswordHash = ph.HashPassword(appUser, "~Someone1");
+        //builder.Entity<IdentityRole>()
+        //    .HasData([
+        //        new IdentityRole
+        //        {
+        //            Name = "SuperAdmin",
+        //            NormalizedName = "SUPERADMIN",
+        //            Id = superAdminRoleId,
+        //            ConcurrencyStamp = superAdminRoleId
+        //        },
+        //        new IdentityRole
+        //        {
+        //            Name = "Admin",
+        //            NormalizedName = "ADMIN",
+        //            Id = adminRoleId,
+        //            ConcurrencyStamp = adminRoleId
+        //        },
+        //        new IdentityRole
+        //        {
+        //            Name = "Trainee",
+        //            NormalizedName = "TRAINEE",
+        //            Id = traineeRoleId,
+        //            ConcurrencyStamp = traineeRoleId
+        //        }
+        //    ]);
 
-        builder.Entity<ApplicationUser>().HasData(appUser);
+        //var appUser = new ApplicationUser
+        //{
+        //    Id = superAdminId,
+        //    Email = "rawal.bishnu@live.com",
+        //    EmailConfirmed = true,
+        //    UserName = "rawal.bishnu@live.com",
+        //    NormalizedUserName = "RAWAL.BISHNU@LIVE.COM"
+        //};
 
-        builder.Entity<IdentityUserRole<string>>()
-            .HasData(new IdentityUserRole<string>
-            {
-                RoleId = superAdminRoleId,
-                UserId = superAdminId
-            });
+        ////set user password
+        //PasswordHasher<ApplicationUser> ph = new();
+        //appUser.PasswordHash = ph.HashPassword(appUser, "~Someone1");
+
+        //builder.Entity<ApplicationUser>().HasData(appUser);
+
+        //builder.Entity<IdentityUserRole<string>>()
+        //    .HasData(new IdentityUserRole<string>
+        //    {
+        //        RoleId = superAdminRoleId,
+        //        UserId = superAdminId
+        //    });
     }
 }
---
+++ AppTechnoSoft.Interns/Data/Models/Instrcutor.cs
@@ -0,0 +1,10 @@
+﻿namespace AppTechnoSoft.Interns.Data.Models;
+public class Instrcutor
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Resume { get; set; } = string.Empty;
+    public string? ProfilePath { get; set; }
+
+    public List<Tag>? Tags { get; set; } = [];
+}

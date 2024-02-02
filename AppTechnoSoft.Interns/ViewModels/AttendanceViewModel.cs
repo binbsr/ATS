feadcc1a7f75bd031e01275964824bfd23a4a07a
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-02-02 23:10:28 +0545

    Implement task logger for intern

---
+++ AppTechnoSoft.Interns/ViewModels/AttendanceViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AppTechnoSoft.Core.ViewModels;
+public class AttendanceViewModel
+{
+    public DateTime? Date { get; set; }
+    public TimeSpan? PunchIn { get; set; }
+    public TimeSpan? PunchOut { get; set; }
+    public string Task { get; set; } = string.Empty;
+    public string StudentId { get; set; } = string.Empty;
+}

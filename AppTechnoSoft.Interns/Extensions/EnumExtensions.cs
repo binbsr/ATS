79f2502425a550ca5794f38f39a3420e352eae4f
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-02-06 19:07:42 +0545

    Create enum extension for display texts

---
+++ AppTechnoSoft.Interns/Extensions/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿using AppTechnoSoft.Interns.Data.Enums;
+
+namespace AppTechnoSoft.Interns.Extensions;
+public static class EnumExtensions
+{
+    public static string GetGeneralProgText(this GeneralProgrammingExperience experience) => experience switch
+    {
+        GeneralProgrammingExperience.None => "Know nothing about programming",
+        GeneralProgrammingExperience.KnowBasicProgrammingLogic => "Know basic logic (console read/write, conditions, loops etc.)",
+        GeneralProgrammingExperience.ConfidenceOnALanguage => "Can code on any of C/C++/C#/Java/Python/JavaScript/etc.",
+        GeneralProgrammingExperience.ConfidenceOnMultipleLanguageParadigms => "Can code on two or more programming languages",
+        _ => string.Empty
+    };
+
+    public static string GetWebExperienceText(this WebExperience experience) => experience switch
+    {
+        WebExperience.None => "No knowledge at all",
+        WebExperience.LittleHtmlCssAndJs => "Know little Html, CSS and JS",
+        WebExperience.DevelopedHobbyWebApp => "Developed small hobby app",
+        WebExperience.DevelopedCommercialApp => "Developed Commercial app for users",
+        _ => string.Empty
+    };
+
+    public static string GetDbExperienceText(this DbExperience experience) => experience switch
+    {
+        DbExperience.None => "No knowledge at all",
+        DbExperience.KnowLittleSql => "Know little SQL and relational databases",
+        DbExperience.ComfortableWithSql => "Comfortable with SQL and relational databases",
+        DbExperience.DesignedDbForApp => "Desinged database (schemas, constraints etc.) for app",
+        _ => string.Empty
+    };
+}

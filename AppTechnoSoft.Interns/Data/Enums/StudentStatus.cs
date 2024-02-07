cdaa975f0e84d5e46a27ccc422143b6347e573ee
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-02-07 10:27:04 +0545

    Teams audit timestamps and refactorings

--- AppTechnoSoft.Interns/Data/Enums/StudentStatus.cs
+++ AppTechnoSoft.Interns/Data/Enums/StudentStatus.cs
@@ -4,8 +4,8 @@ public enum Status
     Applied = 1,        // When application received
     Enrolled,           // When assigned to appropriate batch
     PendingFee,         // When fee is pending for individual
-    Learning,           // On training now
-    ProjectStarted,     // Assinged to a team
+    Learning,           // On training now and fee paid
+    ProjectStarted,     // When assinged to a team
     ProjectSubmitted,   // Project done
-    Done                // Internship done
+    Done                // Internship done, e-certificate shared
 }

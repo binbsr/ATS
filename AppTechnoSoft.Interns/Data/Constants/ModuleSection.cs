56524cf9ba602bc2ed15c423011ebfa62a5c33aa
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-03-03 22:51:17 +0545

    Add sections on modules page

---
+++ AppTechnoSoft.Interns/Data/Constants/ModuleSection.cs
@@ -0,0 +1,16 @@
+﻿namespace AppTechnoSoft.Interns.Data.Constants;
+public static class ModuleSection
+{
+    public const string Common = "Common";
+    public const string Sdk = "SDK";
+    public const string DataAccess = "Data Access";
+    public const string Web = "Web";
+    public const string Security = "Security";
+    public const string Devops = "DevOps";
+    public const string Networking = "Networking";
+    public const string Ml = "ML";
+    public const string Infra = "Infra";
+    public const string Requirements = "Requirements";
+    public const string TechStack = "Tech Stack";
+    public const string TechRefresh = "Tech Refresh";
+}
---
+++ AppTechnoSoft.Interns/Data/Constants/RootFilter.cs
@@ -0,0 +1,7 @@
+﻿namespace AppTechnoSoft.Interns.Data.Constants;
+public static class RootFilter
+{
+    public const string Internship = "Internship";
+    public const string Professional = "Professional Training";
+    public const string Corporate = "Corporate Training";
+}

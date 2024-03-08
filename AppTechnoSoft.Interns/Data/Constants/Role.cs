32ab4862c89657fe6e149384481b11683e258328
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-03-08 22:49:06 +0545

    Remove magic strings for role

---
+++ AppTechnoSoft.Interns/Data/Constants/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace AppTechnoSoft.Interns.Data.Constants;
+public class Role
+{
+    public const string Admin = nameof(Admin);
+    public const string SuperAdmin = nameof(SuperAdmin);
+    public const string Trainee = nameof(Trainee);
+    public const string Employee = nameof(Employee);
+}

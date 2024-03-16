2ffac954bd3ea82ae64cdc6b548aa0101f7daef5
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-03-16 23:56:24 +0545

    Minor refactoring

--- AppTechnoSoft.Interns/Data/Models/Instructor.cs
+++ AppTechnoSoft.Interns/Data/Models/Instructor.cs
@@ -7,9 +7,9 @@ public class Instructor
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public string Phone { get; set; } = string.Empty;
+    public string? Phone { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
-    public string Organization { get; set; } = string.Empty;
+    public string? Organization { get; set; } = string.Empty;
     public string Resume { get; set; } = string.Empty;
     public string? ProfilePath { get; set; }
     public bool Publish { get; set; } = true;

df9e3baf84110e38ccb8e89cac150bc8710ff699
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-03-22 09:02:50 -0400

    Add tech subfilters

---
+++ AppTechnoSoft.Interns/Data/Constants/BackendTech.cs
@@ -0,0 +1,8 @@
+﻿namespace AppTechnoSoft.Interns.Data.Constants;
+public class BackendTech
+{
+    public const string NET = ".NET";
+    public const string Java = "Java";
+    public const string Python = "Python";
+    public const string Node = "Node.js";
+}

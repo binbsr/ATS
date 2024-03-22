9e7bf5e2af647f04f4d969a7c3fb540624e78abf
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-03-22 23:39:50 +0545

    Added subfilters for modules on tech backend

--- AppTechnoSoft.Interns/Data/Constants/BackendTech.cs
+++ AppTechnoSoft.Interns/Data/Constants/BackendTech.cs
@@ -2,7 +2,8 @@
 public class BackendTech
 {
     public const string NET = ".NET";
-    public const string Java = "Java";
-    public const string Python = "Python";
+    public const string Java = nameof(Java);
+    public const string Python = nameof(Python);
     public const string Node = "Node.js";
+    public const string All= nameof(All);
 }

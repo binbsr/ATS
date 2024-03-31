6babc500214b337370cdbbe52cbb292c758c1b73
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-03-31 21:51:42 +0545

    Turn in assignments to batch feature

--- AppTechnoSoft.Interns/ViewModels/AssignmentViewModel.cs
+++ AppTechnoSoft.Interns/ViewModels/AssignmentViewModel.cs
@@ -7,7 +7,7 @@ public class AssignmentViewModel
     public string Title { get; set; } = string.Empty;
     public string? Details { get; set; }
     public string ModuleName { get; set; } = string.Empty;
-    public string Created { get; set; }
+    public string Created { get; set; } = string.Empty;
     public string CreatedBy { get; set; } = string.Empty;
     public bool ShowDetails { get; set; }
 }

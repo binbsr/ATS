08d38d9850e668ab1f1f10a3e66088a2497e2605
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-04-02 11:15:01 +0545

    Add competency and status widgets on dashboard

--- AppTechnoSoft.Interns/Data/Enums/StudentStatus.cs
+++ AppTechnoSoft.Interns/Data/Enums/StudentStatus.cs
@@ -1,11 +1,12 @@
 ﻿namespace AppTechnoSoft.Interns.Data.Enums;
 public enum Status
 {
-    Applied = 1,        // When application received
-    Enrolled,           // When assigned to appropriate batch
-    PendingFee,         // When fee is pending for individual
-    Learning,           // On training now and fee paid
-    ProjectStarted,     // When assinged to a team
-    ProjectSubmitted,   // Project done
-    Done                // Internship done, e-certificate shared
+    Applied = 1,                // When application received
+    Enrolled,                   // When assigned to appropriate batch
+    PendingFee,                 // When fee is pending for individual
+    Learning,                   // On training now and fee paid
+    Contribution,               // When assinged to a team
+    Evaluated,                  // Project done
+    Documentation,              // Internship documentation and reports
+    Completed                   // Internship done, e-certificate shared
 }

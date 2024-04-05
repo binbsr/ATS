30e729cffa381fe4e594426f6996cd95dae58532
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-04-05 23:02:02 +0545

    Add studentassignmentviewmodel and mapper

---
+++ AppTechnoSoft.Interns/ViewModels/StudentAssignmentViewModel .cs
@@ -0,0 +1,20 @@
+﻿using AppTechnoSoft.Interns.Data.Enums;
+
+namespace AppTechnoSoft.Core.ViewModels;
+public class StudentAssignmentViewModel
+{
+    public int Id { get; set; }
+    public AssignmentStatus Status { get; set; }
+    public string? SolutionUrl { get; set; }
+    public string? Comment { get; set; }
+    public string? Deadline { get; set; }
+    public byte Evaluation { get; set; } = 0;  //Out of 100
+
+    public int InstructorId { get; set; }
+    public string InstructorName { get; set; } = string.Empty;
+    public string StudentId { get; set; } = string.Empty;
+    public string StudentName { get; set; } = string.Empty;
+    public int AssignmentId { get; set; }
+    public string AssignmentTitle { get; set; } = string.Empty;
+    public string AssignmentBody { get; set; } = string.Empty;
+}

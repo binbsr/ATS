1081aae35223a1e723fa932a2cbdbd05d86fd8e9
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-04-24 15:25:33 +0545

    Create public profile for trainee and refactoring

---
+++ AppTechnoSoft.Interns/ViewModels/TraineeViewModel.cs
@@ -0,0 +1,7 @@
+﻿using AppTechnoSoft.Interns.Data.Enums;
+
+namespace AppTechnoSoft.Core.ViewModels;
+public record TraineeViewModel(string Id, string Name, string AvatarPath, string Program, string College)
+{
+    public Status Status { get; set; }
+}

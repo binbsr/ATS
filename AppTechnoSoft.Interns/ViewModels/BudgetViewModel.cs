70f805e4a649cbef694232c2df28b14083a8d347
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-05-09 12:22:08 +0545

    Add statement page

--- AppTechnoSoft.Interns/ViewModels/BudgetViewModel.cs
+++ AppTechnoSoft.Interns/ViewModels/BudgetViewModel.cs
@@ -6,6 +6,7 @@ public class BudgetViewModel
     public string Title { get; set; } = string.Empty;
     public float? Credit { get; set; }
     public float? Debit { get; set; }
+    public float? Balance { get; set; }
     public string Notes { get; set; } = string.Empty;
     public DateTime? Date { get; set; }
     public BudgetStatus Status { get; set; } = BudgetStatus.None;

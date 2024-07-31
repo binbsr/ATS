eb5b351ba7e2d00095f10dacf95e0e1ca583e085
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-07-31 23:48:08 +0545

    Modifications around trainee and employee navigations/pages

--- AppTechnoSoft.Interns/Data/Constants/Role.cs
+++ AppTechnoSoft.Interns/Data/Constants/Role.cs
@@ -6,6 +6,7 @@ public class Role
     public const string SuperAdmin = nameof(SuperAdmin);
     public const string Trainee = nameof(Trainee);
     public const string Employee = nameof(Employee);
+    public const string Trainees = $"{Trainee},{Employee}";
     public const string Admins = $"{Admin},{SuperAdmin}";
     public const string AdminsAndInstructor = $"{Admin},{SuperAdmin},{Instructor}";
 }
--- AppTechnoSoft.Interns/ViewModels/StudentLightViewModel.cs
+++ AppTechnoSoft.Interns/ViewModels/StudentLightViewModel.cs
@@ -1,3 +1,3 @@
 ﻿namespace AppTechnoSoft.Core.ViewModels;
-public record StudentLightViewModel(string Name, string UserId, string ProfilePath, string TeamName, string BatchName);
+public record StudentLightViewModel(string Name, string UserId, string ProfilePath, string TeamName="", string BatchName="");
 

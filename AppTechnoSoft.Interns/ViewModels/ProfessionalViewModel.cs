82f405bfa2d2a531e0d0d30c4221ec83943b95b2
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-08-12 00:36:11 +0545

    Refactor accomplishment pages

---
+++ AppTechnoSoft.Interns/ViewModels/ProfessionalViewModel.cs
@@ -0,0 +1,7 @@
+﻿using AppTechnoSoft.Interns.Data.Enums;
+
+namespace AppTechnoSoft.Core.ViewModels;
+public record ProfessionalViewModel(string Id, string Name, string AvatarPath, string Training, string Organization)
+{
+    public Status Status { get; set; }
+}
---
+++ AppTechnoSoft.Interns/ViewModels/TraineeTask.cs
@@ -0,0 +1,5 @@
+﻿using AppTechnoSoft.Interns.Data.Enums;
+
+namespace AppTechnoSoft.Core.ViewModels;
+public record TraineeTask(DateTime Date, string Task, string Details, TaskType TaskType);
+

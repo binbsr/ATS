0125cf18d90412650fca31f5b7b88c661bfe793b
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-08-17 23:05:20 +0545

    Add trainee assignments performance in training summary  page

--- AppTechnoSoft.Interns/ViewModels/ProfessionalViewModel.cs
+++ AppTechnoSoft.Interns/ViewModels/ProfessionalViewModel.cs
@@ -1,7 +1,14 @@
 ﻿using AppTechnoSoft.Interns.Data.Enums;
+using AppTechnoSoft.Interns.Data.Models;
 
 namespace AppTechnoSoft.Core.ViewModels;
-public record ProfessionalViewModel(string Id, string Name, string AvatarPath, string Training, string Organization)
+public record ProfessionalViewModel(string Id, 
+    string Name, 
+    string AvatarPath, 
+    string Training = "", 
+    string Organization = "",
+    StudentAssignment[]? Assignments = null,
+    Project[]? Projects = null)
 {
     public Status Status { get; set; }
 }

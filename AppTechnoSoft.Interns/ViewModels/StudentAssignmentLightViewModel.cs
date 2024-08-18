4a7adb916213e8ce38a3534e842fd48125f58f8f
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-08-18 22:58:23 +0545

    Add professionals profile pages

---
+++ AppTechnoSoft.Interns/ViewModels/StudentAssignmentLightViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AppTechnoSoft.Interns.Data.Enums;
+
+namespace AppTechnoSoft.Core.ViewModels;
+public record StudentAssignmentLightViewModel(
+    string Title,
+    DateTime? ReturnDate,
+    DateTime? Deadline,
+    byte Score,
+    AssignmentStatus Status)
+{
+}

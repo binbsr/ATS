17ef8c56f4256e91bc1b0d1fd25daaece039a3e2
Lemon <sabinbasi111@gmail.com>
2024-08-22 15:58:47 +0545

    add script for loan Table

---
+++ AppTechnoSoft.Interns/Data/Scripts/AddLoanTracker.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Loans] (
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [Borrower]       NVARCHAR (MAX) NOT NULL,
+    [Lender]         NVARCHAR (MAX) NOT NULL,
+    [Principal]      REAL           NOT NULL,
+    [RepaymentDate]  DATETIME2 (7)  NULL,
+    [Created]        DATETIME2 (7)  NULL,
+    [CreatedBy]      NVARCHAR (MAX) NULL,
+    [LastUpdated]    DATETIME2 (7)  NULL,
+    [LastUpdatedBy]  NVARCHAR (MAX) NULL,
+    [IssueDate]      DATETIME2 (7)  NULL,
+    [InterestRate]   REAL           NOT NULL,
+    [CompanyLending] BIT            NOT NULL
+);

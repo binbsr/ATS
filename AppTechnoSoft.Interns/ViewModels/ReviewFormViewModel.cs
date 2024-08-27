8a832b43168611834b431095054b36b345701e2e
Lemon <sabinbasi111@gmail.com>
2024-08-27 14:39:07 +0545

    reviewFormMapper and viewModel

---
+++ AppTechnoSoft.Interns/ViewModels/ReviewFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AppTechnoSoft.Interns.Data.Models;
+
+namespace AppTechnoSoft.Interns.ViewModels;
+
+public class ReviewFormViewModel
+{
+    public ReviewFormViewModel() => Id = Guid.NewGuid().ToString();
+
+    public string Id { get; set; }
+    public string Title { get; set; } = "";
+    public DateTime? Expiration { get; set; }
+
+    public int InstructorId { get; set; }
+    public Instructor? Instructor { get; set; }
+
+    public int TrainingId { get; set; }
+    public Training? Training { get; set; }
+    public string Link { get; set; } = "";
+    public int ReviewCount { get; set; }
+    public string Created { get; set; } = string.Empty;
+    public string CreatedBy { get; set; } = string.Empty;
+    public DateTime LastUpdated { get; set; }
+    public string LastUpdatedBy { get; set; } = string.Empty;
+}

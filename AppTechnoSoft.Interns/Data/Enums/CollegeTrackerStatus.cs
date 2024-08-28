2a3f9d16088d230fba0605d8f1981c4fe5c1a56b
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-08-28 18:26:29 +0545

    Add tracker models

---
+++ AppTechnoSoft.Interns/Data/Enums/CollegeTrackerStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace AppTechnoSoft.Interns.Data.Enums;
+public enum CollegeTrackerStatus
+{
+    None = 1,
+    Phoned,
+    StudentOrientationDone,
+    AdminOrientationDone,
+    StudentSelfEnrolled,
+    MOUDone,
+    Phase1Done,
+    Phase2Done,
+    Phase3Done,
+    Phase4Done,
+    Phase5Done,
+    Phase6Done
+}
---
+++ AppTechnoSoft.Interns/Data/Models/CollegeTracker.cs
@@ -0,0 +1,15 @@
+﻿
+using AppTechnoSoft.Interns.Data.Enums;
+
+namespace AppTechnoSoft.Interns.Data.Models;
+public class CollegeTracker : BaseEntity
+{
+    public int Id { get; set; }
+    public CollegeTrackerStatus Status { get; set; }
+    public string Contact { get; set; }
+    public string ContactPerson { get; set; }
+    public string Notes { get; set; } = string.Empty;
+
+    public int CollegeId { get; set; }
+    public College? College { get; set; }
+}

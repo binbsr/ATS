091bee588c0606ace7f01a56d57b81a1c068183c
Bishnu Rawal <rawal.bishnu@gmail.com>
2025-12-18 08:23:16 +0545

    Localize date time

---
+++ AppTechnoSoft.Interns/Components/TimeZone/LocalTime.cs
@@ -0,0 +1,44 @@
+﻿using AppTechnoSoft.Interns.Extensions;
+using AppTechnoSoft.Interns.Helpers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace AppTechnoSoft.Interns.Components.TimeZone;
+
+public sealed class LocalTime : ComponentBase, IDisposable
+{
+    [Inject]
+    public TimeProvider TimeProvider { get; set; } = default!;
+
+    [Parameter]
+    public DateTime? DateTime { get; set; }
+
+    protected override void OnInitialized()
+    {
+        if (TimeProvider is BrowserTimeProvider browserTimeProvider)
+        {
+            browserTimeProvider.LocalTimeZoneChanged += LocalTimeZoneChanged;
+        }
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        if (DateTime != null)
+        {
+            builder.AddContent(0, TimeProvider.ToLocalDateTime(DateTime.Value));
+        }
+    }
+
+    public void Dispose()
+    {
+        if (TimeProvider is BrowserTimeProvider browserTimeProvider)
+        {
+            browserTimeProvider.LocalTimeZoneChanged -= LocalTimeZoneChanged;
+        }
+    }
+
+    private void LocalTimeZoneChanged(object? sender, EventArgs e)
+    {
+        _ = InvokeAsync(StateHasChanged);
+    }
+}

244e05dbb8ac258e39696fb8376ceeafef97f0c3
Bishnu Rawal <rawal.bishnu@gmail.com>
2026-01-31 22:44:08 +0545

    Redesign schemas around sites and refactoring

--- AppTechnoSoft.Interns/Data/Models/Sites/Section.cs
+++ AppTechnoSoft.Interns/Data/Models/Sites/Section.cs
@@ -5,6 +5,11 @@ public class Section
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string? Content { get; set; }
+    public int Order { get; set; }
+    public bool Enabled { get; set; } = true;
+
+    public string? SiteId { get; set; }
+    public Site? Site { get; set; }
 
     public List<Widget>? Widgets { get; set; }
 }
---
+++ AppTechnoSoft.Interns/Data/Scripts/sitesections.sql
@@ -0,0 +1,11 @@
+﻿DROP TABLE [SiteSections];
+
+ALTER TABLE [Sections] ADD [Enabled] bit NOT NULL DEFAULT CAST(0 AS bit);
+
+ALTER TABLE [Sections] ADD [Order] int NOT NULL DEFAULT 0;
+
+ALTER TABLE [Sections] ADD [SiteId] nvarchar(450) NULL;
+
+CREATE INDEX [IX_Sections_SiteId] ON [Sections] ([SiteId]);
+
+ALTER TABLE [Sections] ADD CONSTRAINT [FK_Sections_Sites_SiteId] FOREIGN KEY ([SiteId]) REFERENCES [Sites] ([Id]);

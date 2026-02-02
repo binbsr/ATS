14cc4d7dca28a0dd9a129b3d845fd3fb4ca9b556
Bishnu Rawal <rawal.bishnu@gmail.com>
2026-02-02 23:39:30 +0545

    More changes on site sections

---
+++ AppTechnoSoft.Interns/ViewModels/SiteItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AppTechnoSoft.Core.ViewModels;
+public record SiteItemViewModel(int Id, string Label, string LastUpdated = "", string UpdatedBy = "")
+{
+    public bool Enabled { get; set; }
+    public int Order { get; set; }
+    public string? Content { get; set; }
+    public bool Editing { get; set; }
+    public List<SiteItemViewModel>? Widgets { get; set; }
+}
